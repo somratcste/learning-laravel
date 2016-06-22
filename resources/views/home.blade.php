7305cbcca74d0993dc863f023d88bfb9d07223d6
unknown <Somrat@Somrat.(none)>
2016-06-22 19:59:23 +0600

    Using the built in blade template enginee.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.master')
 @section('content')
 	<p>Computer Science and Telecommunication Engineering. Noakhali Science and Technology University. Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.Computer Science and Telecommunication Engineering. Noakhali Science and Technology University.</p>
+	<ul>
+		@for($i=0; $i<5; $i++)
+			@if($i % 2 ==0)
+			<li>Iteration{{$i+1}}</li>
+			@endif
+		@endfor
+	</ul>
 @endsection
